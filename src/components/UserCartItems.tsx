0d874111067926dd69e41299e0efe57a1c733c18
IT21191688 <IT21191688@my.sliit.lk>
2023-12-23 01:56:52 +0530

    user cart remove success

--- src/components/UserCartItems.tsx
+++ src/components/UserCartItems.tsx
@@ -5,18 +5,17 @@ import { useNavigate } from "react-router-dom";
 
 const UserCartItems: React.FC = () => {
   const navigate = useNavigate();
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState<any[]>([]);
 
-  /*
   // Function to increase quantity
-  const increaseQuantity = (index: any) => {
+  const increaseQuantity = (index: number) => {
     const updatedCartItems = [...cartItems];
     updatedCartItems[index].quantity++;
     setCartItems(updatedCartItems);
   };
 
   // Function to decrease quantity
-  const decreaseQuantity = (index: any) => {
+  const decreaseQuantity = (index: number) => {
     const updatedCartItems = [...cartItems];
     if (updatedCartItems[index].quantity > 1) {
       updatedCartItems[index].quantity--;
@@ -24,19 +23,34 @@ const UserCartItems: React.FC = () => {
     }
   };
 
-  // Function to remove an item from the cart
-  const removeCartItem = (index: any) => {
-    const updatedCartItems = [...cartItems];
-    updatedCartItems.splice(index, 1);
-    setCartItems(updatedCartItems);
+  const removeCartItem = async (itemId: string) => {
+    try {
+      const token = localStorage.getItem("token");
+      if (!token) {
+        console.error("Token is missing in localStorage");
+        return;
+      }
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+
+      const response = await axios.delete(
+        `http://localhost:8090/api/v1/cart/removeCartItem/${itemId}`,
+        { headers }
+      );
+
+      if (response.data.isSuccessful === true) {
+        alert("Cart Item Remove Successfully!");
+        fetchItems();
+      }
+    } catch (error) {
+      console.error("Error fetching product data:", error);
+    }
   };
 
-  */
-
-  // Sample useEffect to fetch cart items (Replace with actual API call)
   useEffect(() => {
     fetchItems();
-  }, []); // Empty dependency array to fetch items only once
+  }, []);
 
   const fetchItems = async () => {
     try {
@@ -54,58 +68,66 @@ const UserCartItems: React.FC = () => {
         { headers }
       );
 
-      console.log(response.data.data);
+      console.log(response.data);
 
       // Set the retrieved products in the state
-      setCartItems(response.data.data);
-      //  alert(response.data.data.length);
+      setCartItems(response.data.data.cartItems);
     } catch (error) {
       console.error("Error fetching product data:", error);
     }
   };
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
-      {cartItems.map((item: any, index) => (
-        <div key={index} className="bg-white rounded-lg shadow-md p-4 mb-4">
-          <div className="flex items-center justify-between">
-            <div>
-              <h2 className="text-xl font-semibold">{item.prductname}</h2>
-              <p className="text-gray-600">Price: ${item.price}</p>
+    <div>
+      <div
+        className="container mx-auto px-4 py-8"
+        style={{ maxHeight: "500px", overflowY: "auto" }}
+      >
+        <h1 className="text-3xl font-bold mb-6">Your Cart</h1>
+        {cartItems.map((item: any, index: number) => (
+          <div key={index} className="bg-white rounded-lg shadow-md p-4 mb-4">
+            <div className="flex items-center justify-between">
+              <div>
+                <h2 className="text-xl font-semibold">
+                  {item.productid.productname}
+                </h2>
+                <p className="text-gray-600">
+                  Price: ${item.productid.productprice}
+                </p>
+              </div>
+              <div className="flex items-center">
+                <button
+                  onClick={() => decreaseQuantity(index)}
+                  className="px-3 py-1 bg-gray-200 text-gray-600 font-semibold rounded"
+                >
+                  -
+                </button>
+                <p className="mx-4">{item.quantity}</p>
+                <button
+                  onClick={() => increaseQuantity(index)}
+                  className="px-3 py-1 bg-gray-200 text-gray-600 font-semibold rounded"
+                >
+                  +
+                </button>
+              </div>
             </div>
-            <div className="flex items-center">
+            <div className="flex justify-between items-center mt-4">
+              <p className="text-gray-800 font-semibold">
+                Total: ${item.productid.productprice * item.quantity}
+              </p>
               <button
-                //onClick={() => decreaseQuantity(index)}
-                className="px-3 py-1 bg-gray-200 text-gray-600 font-semibold rounded"
+                onClick={() => removeCartItem(item._id)}
+                className="text-red-500 hover:text-red-700 font-semibold"
               >
-                -
-              </button>
-              <p className="mx-4">{item.quantity}</p>
-              <button
-                //onClick={() => increaseQuantity(index)}
-                className="px-3 py-1 bg-gray-200 text-gray-600 font-semibold rounded"
-              >
-                +
+                Remove
               </button>
             </div>
           </div>
-          <div className="flex justify-between items-center mt-4">
-            <p className="text-gray-800 font-semibold">
-              Total: ${item.price * item.quantity}
-            </p>
-            <button
-              // onClick={() => removeCartItem(index)}
-              className="text-red-500 hover:text-red-700 font-semibold"
-            >
-              Remove
-            </button>
-          </div>
-        </div>
-      ))}
-      {cartItems.length === 0 && (
-        <p className="text-xl text-center">Your cart is empty.</p>
-      )}
+        ))}
+        {cartItems.length === 0 && (
+          <p className="text-xl text-center">Your cart is empty.</p>
+        )}
+      </div>
       <Footer />
     </div>
   );
